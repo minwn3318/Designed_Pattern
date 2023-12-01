5e247f3c83484fc33c4cdd83fb0b37294bcab72c
minwn3318 <123531300+minwn3318@users.noreply.github.com>
2023-12-02 02:06:00 +0900

    프로젝트 엎음

--- Iterator_Pattern/Iterator_Pattern/Iterator_Project.cpp
+++ Iterator_Pattern/Iterator_Pattern/Iterator_Project.cpp
@@ -2,28 +2,107 @@
 #include <vector>
 using namespace std;
 
-class IterShelf {
-
-};
-
-class Con_IterShelf {
-
-};
-
-class Shelf {
-
-};
-
-class Con_Shelf {
-
-};
-
-class Book {
-	const char* title;
+class ItemItr {
 public :
-	Book(const char* t)
+	virtual void first() = 0;
+};
+
+class Self {
+public :
+	virtual ItemItr* createItr() = 0;
+};
+
+class Item {
+protected :
+	string name;
+public :
+	virtual Item* clone() = 0;
+	virtual void setName(string n) = 0;
+	virtual string getName() = 0;
+	virtual void useItem() = 0;
+};
+
+class Book : public Item {
+public :
+	Item* clone()
 	{
-		this->title = t;
+		return new Book();
+	}
+	void setName(string n)
+	{
+		this->name = n;
+	}
+	string getName()
+	{
+		cout << "name : " <<this->name << endl;
+	}
+	void useItem()
+	{
+		cout << "using" << endl;
+	}
+};
+
+class Pot : public Item {
+public:
+	Item* clone()
+	{
+		return new Pot();
+	}
+	void setName(string n)
+	{
+		this->name = n;
+	}
+	string getName()
+	{
+		cout << "name : " << this->name << endl;
+	}
+	void useItem()
+	{
+		cout << "using" << endl;
+	}
+};
+
+class PhotoFrame : public Item {
+public:
+	Item* clone()
+	{
+		return new PhotoFrame();
+	}
+	void setName(string n)
+	{
+		this->name = n;
+	}
+	string getName()
+	{
+		cout << "name : " << this->name << endl;
+	}
+	void useItem()
+	{
+		cout << "using" << endl;
+	}
+};
+
+class ItemItr_con : public Item {
+
+};
+
+class Self_con : public Self {
+
+};
+
+class Person {
+private :
+	Person() {}
+	static Person* instance;
+public :
+	static Person getInstance()
+	{
+		if (instance == nullptr)
+		{
+			instance = new Person();
+		}
+
+		return *instance;
 	}
 };
 
